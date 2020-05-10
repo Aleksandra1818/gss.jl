e70bceac2509919bebe1c0e02fce2b4b7a6e54ba
Aleksandra Pogonska <s168935@pg.edu.pl>
2020-05-10 19:59:50 +0200

    Adiing tests and golden section search function

--- src/gss.jl
+++ src/gss.jl
@@ -1,5 +1,29 @@
 module gss
 
-greet() = print("Hello World!")
+function goldss(f,a,b;ϵ=1e-6,τ=0.618)
+    fa,fb=f(a),f(b)
+    x1,x2=b-(τ)*(b-a),a+(τ)*(b-a)
+    err=Inf
+    while err>ϵ
+        if f(x2)>f(x1)
+            b=x2
+            x2=x1
+            x1=a+(1-τ)*(b-a)
+        elseif f(x2)<f(x1)
+            a=x1
+            x1=x2
+            x2=b-(1-τ)*(b-a)
+        else
+            a=(x1+x2)/2
+            b=a
+        end
+        err=2*abs((b-a)/(b+a))
+    end
+    xsr=(x1+x2)/2
+    wart=f(xsr)
+    return(round(xsr,digits=2),round(wart,digits=3))
+end
+
+export goldss
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,10 @@ using gss
 using Test
 
 @testset "gss.jl" begin
-    # Write your own tests here.
+    f,a,b = x->x^2,-20,20
+	@test goldss(f,a,b)== (0.0,0.0)
+	f,a,b = x-> cos(x),-3.5,0.
+	@test goldss(f,a,b)==(-3.14,-1.0)
+	f,a,b = x->x^4+3*x^3+x^2,-20.,20.
+	@test goldss(f,a,b)==(-2.0,-4.0)
 end
